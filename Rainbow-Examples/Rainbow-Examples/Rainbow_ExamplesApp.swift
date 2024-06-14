7fe736d25fa0d9eae1237e86885028ae1a25cf31
Adam Dahan <adamdahan@Adams-MacBook-Pro.local>
2024-06-14 14:54:05 -0400

    Initial Rainbow examples project, no content.

---
+++ Rainbow-Examples/Rainbow-Examples/Rainbow_ExamplesApp.swift
@@ -0,0 +1,17 @@
+//
+//  Rainbow_ExamplesApp.swift
+//  Rainbow-Examples
+//
+//  Created by Adam Dahan on 2024-06-14.
+//
+
+import SwiftUI
+
+@main
+struct Rainbow_ExamplesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
