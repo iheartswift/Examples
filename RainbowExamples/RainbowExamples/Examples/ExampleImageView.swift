585ba7decc3751721f7cd30d39cec84dce1956f0
Jay Raval <jraval93@gmail.com>
2024-07-14 12:02:29 -0400

    small refactor

--- RainbowExamples/RainbowExamples/Examples/ExampleImageView.swift
+++ RainbowExamples/RainbowExamples/Examples/ExampleImageView.swift
@@ -6,43 +6,31 @@ struct ExampleImageView: View {
     
     private enum RainbowConfiguration {
         static var DiskRainbowImageConfiguration: RainbowImageConfiguration {
-            
-            let gradient = LinearGradient(
-                gradient: Gradient(colors: [.clear, .black]),
-                startPoint: .top,
-                endPoint: .bottom
-            )
+            let gradient = LinearGradient(gradient: Gradient(colors: [.clear, .black]),
+                                          startPoint: .top,
+                                          endPoint: .bottom)
             
             let configuration = Rainbow.RainbowImageConfiguration(source: .disk(name: "art"),
                                                                   gradientOverlay: gradient)
-            
             return configuration
         }
         
-        
-        static let BorderGradient = LinearGradient(
-            gradient: Gradient(colors: [Color(red: 0.87, green: 0.29, blue: 0.61),
-                                        Color(red: 0.98, green: 0.42, blue: 0.41),
-                                        Color(red: 0.99, green: 0.75, blue: 0.29)]),
-            startPoint: .topLeading,
-            endPoint: .bottomTrailing
-        )
+        static let BorderGradient = LinearGradient(gradient: Gradient(colors: [Color(red: 0.87, green: 0.29, blue: 0.61),
+                                                                               Color(red: 0.98, green: 0.42, blue: 0.41),
+                                                                               Color(red: 0.99, green: 0.75, blue: 0.29)]),
+                                                   startPoint: .topLeading,
+                                                   endPoint: .bottomTrailing)
         
         static var RemoteRainbowImageConfiguration: RainbowImageConfiguration {
             let configuration = Rainbow.RainbowImageConfiguration(source: .url( URL(string: "https://stackoverflow.design/assets/img/logos/so/logo-stackoverflow.png")! ))
-            
             return configuration
         }
-        
         
         static var SFRainbowImageConfiguration: RainbowImageConfiguration {
             let configuration = Rainbow.RainbowImageConfiguration(source: .system(name: "globe"))
-            
             return configuration
         }
-        
     }
-    
     
     var body: some View {
         
@@ -69,9 +57,6 @@ struct ExampleImageView: View {
             RainbowImage(configuration: RainbowConfiguration.RemoteRainbowImageConfiguration)
                 .frame(height: 80)
                 .padding()
-            
-            
-            
             
             Spacer()
         }
